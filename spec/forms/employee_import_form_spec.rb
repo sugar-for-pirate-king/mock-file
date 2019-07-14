47d587d1513455c0c06f6b51176b488667b5b021
philiplambok <philiplambok71@gmail.com>
2019-07-14 16:35:34 +0700

    Add mocking file

--- spec/forms/employee_import_form_spec.rb
+++ spec/forms/employee_import_form_spec.rb
@@ -5,10 +5,28 @@ require 'rails_helper'
 RSpec.describe EmployeeImportForm do
   context 'with valid params' do
     it 'returns success message' do
-      # file_path = Rails.root.join('spec', 'fixtures', 'files', 'users-all-valid.xlsx')
-      # form = EmployeeImportForm.new(file: file_path)
-      # form.save
-      # expect(form.success_message).to eq '3 employee imports path'
+      file_path = Rails.root.join(
+        'spec', 'fixtures', 'files', 'users-all-valid.xlsx'
+      )
+      stub_file = FakeFile.new(file_path)
+      form = EmployeeImportForm.new(file: stub_file)
+      form.save
+      expect(form.success_message).to eq '3 employees has been created'
+      expect(User.all.pluck(:username, :age)).to include(
+        ['pquest', 23], ['larapel', 44], ['kimono', 31]
+      )
+    end
+
+    private
+
+    class FakeFile
+      def initialize(path)
+        @path = path
+      end
+
+      def path
+        @path.to_s
+      end
     end
   end
 end
